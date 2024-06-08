b472abdf78d82ccf38456a1cd3f0d5d4c0a888d6
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-08 10:49:16 -0300

    Criei as classes e suas responsabilidades iniciais

---
+++ modelos/biblioteca.py
@@ -0,0 +1,25 @@
+'''
+Biblioteca: Representa a coleção de livros, contendo métodos para adicionar, remover e procurar livros, bem como para emprestar e devolver livros.
+Funcionalidades:
+
+Adicionar livro à biblioteca.
+Remover livro da biblioteca.
+Procurar livro por título ou autor.
+Emprestar livro a um usuário (se disponível).
+Devolver livro.
+'''
+class Biblioteca:
+    def adicionar_livro(self):
+        pass
+
+    def remover_livro(self):
+        pass
+
+    def procurar_livro(self):
+        pass
+
+    def emprestar_livro(self):
+        pass
+    
+    def devolver_livro(self):
+        pass
---
+++ modelos/livro.py
@@ -0,0 +1,17 @@
+import os
+os.system('cls')
+# Livro: Representa um livro na biblioteca, contendo atributos como título, autor, gênero, e status de empréstimo.
+
+class Livro:
+
+    def __init__(self, titulo, autor, genero, status):
+        self._titulo = titulo
+        self._autor = autor
+        self._genero = genero
+        self._status = status
+
+
+
+            
+        
+    
